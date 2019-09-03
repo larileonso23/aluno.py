19116d9c094e1f3ede2db68beae7a074c4ca7ce2
AbnerAbreu <abnerdrift94@gmail.com>
2019-09-03 17:55:41 -0300

    Adicionar atividade aluno

---
+++ landing/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(
+        max_length=255,
+    )
+    idade = models.CharField(
+        max_length=255,
+    )
+    email = models.CharField(
+        max_length=255,
+    )
+    def __str__(self):
+        return self.nome
